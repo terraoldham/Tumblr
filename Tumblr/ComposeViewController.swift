46aa3f254fff34633b2ad477a0240b16b08af919
Terra Oldham <toldham@hearsaycorp.com>
2014-11-10 21:09:55 -0800

    Final Commit
    Final Commit

--- Tumblr/ComposeViewController.swift
+++ Tumblr/ComposeViewController.swift
@@ -15,6 +15,11 @@ class ComposeViewController: UIViewController {
     @IBOutlet weak var linkButton: UIButton!
     @IBOutlet weak var chatButton: UIButton!
     @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var nevermindButton: UIButton!
+    @IBOutlet weak var nevermindView: UIView!
+    @IBOutlet weak var nevermindImageView: UIImageView!
+    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var backgroundView: UIView!
     
     var textButtonFrame: CGFloat!
     var quoteButtonFrame: CGFloat!
@@ -26,14 +31,34 @@ class ComposeViewController: UIViewController {
     @IBAction func nevermindButton(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
         UIView.animateWithDuration(0.5, animations: { () -> Void in
-            self.textButtonFrame = self.textButton.frame.origin.y - 300
+            self.textButtonFrame = self.textButton.frame.origin.y - 500
+            self.textButton.frame.origin.y = self.textButtonFrame
         })
         UIView.animateWithDuration(0.7, animations: { () -> Void in
-            self.chatButtonFrame = self.chatButton.frame.origin.y - 300
+            self.chatButtonFrame = self.chatButton.frame.origin.y - 500
+            self.chatButton.frame.origin.y = self.chatButtonFrame
         })
-        UIView.animateWithDuration(0.5, animations: { () -> Void in
-            self.photoButtonFrame = self.photoButton.frame.origin.y - 300
+        UIView.animateWithDuration(0.5, delay: 0.2, options: nil, animations: { () -> Void in
+            self.chatButtonFrame = self.chatButton.frame.origin.y - 500
+            self.chatButton.frame.origin.y = self.chatButtonFrame
+            }, completion: nil)
+        UIView.animateWithDuration(0.8, animations: { () -> Void in
+            self.linkButtonFrame = self.linkButton.frame.origin.y - 500
+            self.linkButton.frame.origin.y = self.linkButtonFrame
         })
+        dismissViewControllerAnimated(true, completion: nil)
+        UIView.animateWithDuration(0.3, delay: 0.1, options: nil, animations: { () -> Void in
+            self.quoteButtonFrame = self.quoteButton.frame.origin.y - 500
+            self.quoteButton.frame.origin.y = self.quoteButtonFrame
+        }, completion: nil)
+        UIView.animateWithDuration(0.9, animations: { () -> Void in
+            self.videoButtonFrame = self.videoButton.frame.origin.y - 500
+            self.videoButton.frame.origin.y = self.videoButtonFrame
+        })
+        UIView.animateWithDuration(0.2, delay: 0.2, options: nil, animations: { () -> Void in
+            self.photoButtonFrame = self.photoButton.frame.origin.y - 500
+            self.photoButton.frame.origin.y = self.photoButtonFrame
+            }, completion: nil)
     
     }
     override func viewDidLoad() {
--- Tumblr/TabBarViewController.swift
+++ Tumblr/TabBarViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
+    
+    var isPresenting: Bool = true
 
     @IBOutlet weak var homeButton: UIButton!
     @IBOutlet weak var searchButton: UIButton!
@@ -23,6 +25,7 @@ class TabBarViewController: UIViewController {
     var trendingViewController: UIViewController!
     var currentButton: UIButton!
     var currentViewController: UIViewController!
+    var composeVC: UIViewController!
     
     @IBAction func onTabClick(sender: UIButton) {
         currentButton.selected = false
@@ -70,6 +73,75 @@ class TabBarViewController: UIViewController {
         currentButton = homeButton
         currentViewController = homeViewController
         contentView.addSubview(homeViewController.view)
+        
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.4
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        
+        if (isPresenting) {
+            var composeVC = toViewController as ComposeViewController
+            
+            composeVC.textButton.frame.origin.y = 490
+            composeVC.quoteButton.frame.origin.y = 550
+            composeVC.photoButton.frame.origin.y = 575
+            composeVC.linkButton.frame.origin.y = 1900
+            composeVC.chatButton.frame.origin.y = 2500
+            composeVC.videoButton.frame.origin.y = 1000
+            
+            
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                
+                composeVC.view.alpha = 1
+                composeVC.backgroundView.alpha = 0.7
+                composeVC.nevermindButton.alpha = 1
+                composeVC.textButton.frame.origin.y = 181
+                composeVC.quoteButton.frame.origin.y = 181
+                composeVC.photoButton.frame.origin.y = 181
+                composeVC.linkButton.frame.origin.y = 297
+                composeVC.chatButton.frame.origin.y = 297
+                composeVC.videoButton.frame.origin.y = 297
+                
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+            }
+        
+        } else {
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    fromViewController.view.removeFromSuperview()
+            }
+        }
+    }
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        var destinationVC = segue.destinationViewController as UIViewController
+        destinationVC.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationVC.transitioningDelegate = self
         
     }
 
